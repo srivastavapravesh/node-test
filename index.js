f04b3a1b850acda40161c5abd95fa9e917c90ec7
srivastavapravesh <53289870+srivastavapravesh@users.noreply.github.com>
2023-04-18 18:56:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,27 @@
-const http = require('http');
-const https = require('https');
+const express = require('express');
 const bodyParser = require('body-parser');
 
-// Create a server to receive POST requests from SNS
-const server = http.createServer((req, res) => {
-  if (req.method === 'POST' && req.url === '/') { // Route for handling POST requests to the root URL
-    let body = '';
-    req.on('data', (chunk) => {
-      body += chunk;
-    });
-    req.on('end', () => {
-      try {
-        const data = JSON.parse(body);
-        console.log('Received message:', data);
-        res.end('OK');
-      } catch (error) {
-        console.error('Error parsing JSON:', error);
-        res.statusCode = 400;
-        res.end('Bad Request');
-      }
-    });
-  } else if (req.method === 'GET' && req.url === '/hello') { // Route for handling GET requests to /hello
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!');
-  } else { // Default route handler for all other requests
-    res.statusCode = 405;
-    res.end('Method Not Allowed');
-  }
+const app = express();
+app.use(bodyParser.json());
+
+// Route handler for GET /hello
+app.get('/hello', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Route handler for POST /
+app.post('/', (req, res) => {
+  const data = req.body;
+  console.log('Received message:', data);
+  res.send('OK');
+});
+
+// Default route handler for all other requests
+app.use((req, res) => {
+  res.status(405).send('Method Not Allowed');
 });
 
 // Start the server on port 3000
-server.listen(3000, () => {
+app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
